7b69c25000da71e9615c46223e2dc0378d9c4d32
Aerynnnn <159232615+Aerynnnn@users.noreply.github.com>
2024-04-27 11:55:21 +0800

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: "Your token here",
-    prefix: "Your prefix here"
+    token: "MTE2ODc4ODgyOTEzMDQ2NTMyMg.GaUZjN.BJzcB9S7cHs-jjHqD0X9DIaERRhSX8UfVFUISU",
+    prefix: ",,"
 };
 
 /* remove .example from the name of this file */
